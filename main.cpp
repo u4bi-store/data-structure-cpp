14434c259f34698244420e32876ea985ebc6d906
u4bi <u4bi@u4bi.com>
2016-12-03 13:33:17 +0000

    data structure-cpp init

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+void line(){ cout << endl; }
+
+int main() {
+    cout << "u4bi c++ data structure" <<endl;
+    line();
+    
+    return 0;
+}
